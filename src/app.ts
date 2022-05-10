33b14bf966569babf9385f0eb95471c8cb045d7e
rapzaDev <rapzadev@gmail.com>
2022-05-09 22:07:37 -0300

    Node.js MongoDB Connection

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,18 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
 import routes from './routes';
+
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .catch((err) => console.log(`DB: ${err}`));
 
 export default app;
